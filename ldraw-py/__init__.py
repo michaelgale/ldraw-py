7dd93e13232bcf20b13ed44e985b17d9926c6219
michaelgale <michael@fxbricks.com>
2019-07-08 22:43:07 -0400

    Conflicted commit before merge

--- ldraw-py/__init__.py
+++ ldraw-py/__init__.py
@@ -1,5 +1,12 @@
 """ldraw-py - A utility package for creating, modifying, and reading LDraw files and data structures."""
 
+import os
+
+__project__ = 'ldraw'
 __version__ = '0.1.0'
-__author__ = 'Michael Gale <michael@fxbricks.com>'
-__all__ = []
+
+VERSION = __project__ + '-' + __version__
+
+script_dir = os.path.dirname(__file__)
+
+from .ldraw import *
